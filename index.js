7961cc69b391da1bb15d3375a438cd2f221a322e
walsc024 <66418697+walsc024@users.noreply.github.com>
2021-01-08 19:26:56 +0000

    added title question

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
+const inquirer = require('inquirer')
 // array of questions for user
 const questions = [
-
+{
+    type: "input",
+    name: "title",
+    message: "What is the title of this application?"
+}
 ];
 
 // function to write README file
@@ -9,7 +14,9 @@ function writeToFile(fileName, data) {
 
 // function to initialize program
 function init() {
-
+inquirer.prompt(questions).then((userAnswers) => {
+console.log(userAnswers)
+})
 }
 
 // function call to initialize program
